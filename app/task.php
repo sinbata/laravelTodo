671760399e9916209729b3151974958a4599c3fa
sinbata <batabata4096@gmail.com>
2019-07-04 18:23:09 +0900

    初期化

--- app/task.php
+++ app/task.php
@@ -5,11 +5,4 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class task extends Model
-{
-    protected $table = 'tasks';
-    protected $filltable = [
-        'name'
-    ]
-    protected $guarded;
-    \App \Author::create (['name' => 'aiueo']);
-}
+{ }
