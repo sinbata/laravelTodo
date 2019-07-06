17323fc04def2930b72c86ddd600ee484f94c4cf
sinbata <batabata4096@gmail.com>
2019-07-06 14:19:14 +0900

    controllerの名前変更

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Http\Request;
 
 //use Illuminate\Validation\Validator;
 
-Route::get('/', 'Todo\displayTodo@index');
+Route::get('/', 'Todo\TasksController@index');
 
-Route::post('/task', 'Todo\displayTodo@store');
+Route::post('/task', 'Todo\TasksController@store');
 
-Route::delete('/task/{id}', 'Todo\displayTodo@destroy');
+Route::delete('/task/{id}', 'Todo\TasksController@destroy');
 
-Route::patch('/task/{id}', 'Todo\displayTodo@update');
+Route::patch('/task/{id}', 'Todo\TasksController@update');
